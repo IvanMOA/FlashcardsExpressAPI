e7d4a090a209dd2d2f30b516d04ba6ca953f179f
Ivan <ax3l.imo@gmail.com>
2021-01-08 18:27:48 -0600

    Tests for POST /item updated

--- src/routes/itemRouter.ts
+++ src/routes/itemRouter.ts
@@ -5,14 +5,22 @@ import { body } from "express-validator";
 // import authenticateWithJWT from "../middleware/authenticateWithJWT";
 const itemRouter: Router = express.Router();
 
-const isNotNeg = (x : number) => x >= 0
+const isNotNeg = (x: number) => x >= 0;
 
 // TODO : Should include error codes with a custom msg
 const validateItem = [
-  body("description").isString().isLength({min: 10, max: 300}).withMessage("IC001:Description lenght must lay between 10 and 300 characters"),
+  body("description")
+    .isString()
+    .isLength({ min: 10, max: 300 })
+    .withMessage(
+      "IC001:Description lenght must lay between 10 and 300 characters"
+    ),
   body("stock").isNumeric().custom(isNotNeg),
-  body("name").isString().isLength({min: 0}),
-  body("image").isString().isLength({min: 0}).withMessage("BR004:Field image cannot be of length 0"),
+  body("name").isString().isLength({ min: 0 }),
+  body("image")
+    .isString()
+    .isLength({ min: 0, max: 300 })
+    .withMessage("BR004:Field image cannot be of length 0"),
   body("price").isNumeric().custom(isNotNeg),
 ];
 
--- src/tests/item.test.ts
+++ src/tests/item.test.ts
@@ -22,25 +22,77 @@ describe("GET /item", () => {
 });
 
 describe("POST /item", () => {
-    let mockItem = {
-      description: "TestTestTest",
-      name: "Test@test.com",
-      stock: 2,
-      image: "Test",
-      price: 50,
-    };
+  let mockItem = {
+    description: "TestTestTest",
+    name: "Test@test.com",
+    stock: 2,
+    image: "Test",
+    price: 50,
+  };
   it("should create an item correctly", (done) => {
-    let niceItem = {...mockItem}
-    request(app).post("/item").send(niceItem).expect(201).expect(mockItem, done);
+    let niceItem = { ...mockItem };
+    request(app)
+      .post("/item")
+      .send(niceItem)
+      .expect(201)
+      .expect(mockItem, done);
   });
   it("should not create an item if name is not valid", (done) => {
-    let noNameItem = {...mockItem}
-    noNameItem.name = null
-    request(app).post("/item").send(noNameItem).expect(400, done);
-    noNameItem.name = ""
-    request(app).post("/item").send(noNameItem).expect(400, done);
+    let noNameItem = { ...mockItem };
+    noNameItem.name = null;
+    request(app).post("/item").send(noNameItem).expect(400);
+    noNameItem.name = "";
+    request(app).post("/item").send(noNameItem).expect(400);
     // @ts-ignore
-    noNameItem.name = 1
+    noNameItem.name = 1;
     request(app).post("/item").send(noNameItem).expect(400, done);
+  });
+  it("should not create an item if description is not valid", (done) => {
+    let noDescItem = { ...mockItem };
+    noDescItem.description = null;
+    request(app).post("/item").send(noDescItem).expect(400);
+    noDescItem.description = "";
+    request(app).post("/item").send(noDescItem).expect(400);
+    noDescItem.description = "123456789";
+    request(app).post("/item").send(noDescItem).expect(400);
+    // 300+ description length
+    noDescItem.description =
+      "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII";
+    request(app).post("/item").send(noDescItem).expect(400);
+    // @ts-ignore
+    noDescItem.description = 1;
+    request(app).post("/item").send(noDescItem).expect(400, done);
+  });
+  it("should not create an item if stock is not valid", (done) => {
+    let noStockItem = { ...mockItem };
+    noStockItem.stock = null;
+    request(app).post("/item").send(noStockItem).expect(400);
+    noStockItem.stock = -1;
+    request(app).post("/item").send(noStockItem).expect(400);
+    // @ts-ignore
+    noStockItem.stock = "";
+    request(app).post("/item").send(noStockItem).expect(400, done);
+  });
+  it("should not create an item if image is not valid", (done) => {
+    let noImageItem = { ...mockItem };
+    noImageItem.image = null;
+    request(app).post("/item").send(noImageItem).expect(400);
+    // @ts-ignore
+    noImageItem.image = -1;
+    request(app).post("/item").send(noImageItem).expect(400);
+    // @ts-ignore
+    noImageItem.image =
+      "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII";
+    request(app).post("/item").send(noImageItem).expect(400, done);
+  });
+  it("should not create an item if price is not valid", (done) => {
+    let noPriceItem = { ...mockItem };
+    noPriceItem.price = null;
+    request(app).post("/item").send(noPriceItem).expect(400);
+    noPriceItem.price = -1;
+    request(app).post("/item").send(noPriceItem).expect(400);
+    // @ts-ignore
+    noPriceItem.price = "";
+    request(app).post("/item").send(noPriceItem).expect(400, done);
   });
 });
