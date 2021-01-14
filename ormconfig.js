cc4e849eb6c0b7ecdd4fc7191204b0cdbfe40b24
IvanMOA <52722893+IvanMOA@users.noreply.github.com>
2021-01-14 17:02:19 -0600

    Fixed problems with TypeORM paths when running mpm test vs npm dev

--- ormconfig.js
+++ ormconfig.js
@@ -1,3 +1,17 @@
+require('dotenv').config()
+
+// Typescript must be compiled to js before running
+// migrations from  TypeORM (npx typeorm migrations:run)
+// and to run tests we need to point to the ts files
+
+let entitiesPath = "dist/entity/**/*.js"
+let migrationsPath = "dist/migrations/**/*.js"
+
+if(process.env.NODE_ENV === 'TEST'){
+  entitiesPath = "src/entity/**/*.ts"
+  migrationsPath = "src/migrations/**/*.ts"
+}
+
 module.exports = {
   name: "default",
   type: "postgres",
@@ -7,10 +21,10 @@ module.exports = {
   password: "example",
   database: "shop",
   logging: false,
-  entities: ["src/entity/**/*.ts"],
-  migrations: ["src/migrations/**/*.ts"],
+  entities: [entitiesPath],
+  migrations: [migrationsPath],
   cli: {
     entitiesDir: "src/entity",
-    migrationsDir: "src/migrations",
+    migrationsDir: "dist/migrations",
   },
 };
